353bd829be78ba144b1c92d3ee1fabac8f6f223e
masiv <masiviady7@gmail.com>
2023-01-24 14:15:36 +0100

    exo2

--- index.js
+++ index.js
@@ -1,15 +1,26 @@
-var foo = 1;
-let foobar = 10;
-(function () {
-  console.log(foo);
-  console.log(baz);
-  var foo = 2;
-  let baz = 3;
-  const bar = 4;
-  console.log(foobar);
-})();
+// var foo = 1;
+// let foobar = 10;
+// (function () {
+//   console.log(foo);
+//   console.log(baz);
+//   var foo = 2;
+//   let baz = 3;
+//   const bar = 4;
+//   console.log(foobar);
+// })();
 
-console.log(foo);
-console.log(bar);
-console.log(baz);
-console.log(foobar);
+// console.log(foo);
+// console.log(bar);
+// console.log(baz);
+// console.log(foobar);
+
+// Exo II
+function creerFontion() {
+  const nom = "Mozila";
+  function afficheNom() {
+    console.log(nom);
+  }
+  return afficheNom;
+}
+const maFonction = creerFontion();
+maFonction();
